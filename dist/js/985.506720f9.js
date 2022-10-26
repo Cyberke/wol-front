0e1edbaef784862a379a9a681f1108601161e40d
Cyberke <70345298+Cyberke@users.noreply.github.com>
2022-10-26 16:46:24 +0300

    Delete dist directory

--- dist/js/985.506720f9.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[985],{7985:function(e,c,n){n.r(c);const t={},s=t;c["default"]=s}}]);
-//# sourceMappingURL=985.506720f9.js.map
